f373f44a821933a2984788c01070afafd1871c67
BalasaravananV <balasaravanan.myname@gmail.com>
2023-09-05 17:52:19 +0530

    Files uploads

---
+++ index.php
@@ -0,0 +1,424 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Inexoft Website Demo</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <!-- <link href="assets/img/favicon.png" rel=""> -->
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Medicio
+  * Updated: Aug 30 2023 with Bootstrap v5.3.1
+  * Template URL: https://bootstrapmade.com/medicio-free-bootstrap-theme/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Top Bar ======= -->
+  <!-- <div id="topbar" class="d-flex align-items-center fixed-top">
+    <div class="container d-flex align-items-center justify-content-center justify-content-md-between">
+      <div class="align-items-center d-none d-md-flex">
+        <i class="bi bi-clock"></i> Monday - Saturday, 8AM to 10PM
+      </div>
+      <div class="d-flex align-items-center">
+        <i class="bi bi-phone"></i> Call us now +1 5589 55488 55
+      </div>
+    </div>
+  </div> -->
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+      <a href="index.html" class="logo"><img src="assets/img/logo.png" alt=""></a>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <h1 class="logo me-auto"><a href="index.html">Medicio</a></h1> -->
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a class="nav-link scrollto " href="#">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">About Us</a></li>
+          <li><a class="nav-link scrollto" href="#service">Services</a></li>
+          <li><a class="nav-link scrollto" href="#odoo">Products</a></li>
+          <li><a class="nav-link scrollto" href="#">Careers</a></li>
+          <li><a class="nav-link scrollto" href="#contact">Get in Touch</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+     </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+
+      <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+
+      <div class="carousel-inner" role="listbox">
+
+        <!-- Slide 1 -->
+        <div class="carousel-item active" style="background-image: url(assets/img/slide/slider1.jpg);">
+          <div class="container">
+            <h2>Odoo Customization</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <a href="#about" class="btn-get-started scrollto">Read More</a>
+          </div>
+        </div>
+
+        <!-- Slide 2 -->
+        <div class="carousel-item" style="background-image: url(assets/img/slide/slider2.jpg);">
+          <div class="container">
+            <h2>Digital Transformation/Business Intelligence</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <a href="#about" class="btn-get-started scrollto">Read More</a>
+          </div>
+        </div>
+
+        <!-- Slide 3 -->
+        <div class="carousel-item" style="background-image: url(assets/img/slide/slider3.jpg)">
+          <div class="container">
+            <h2>IT & Business Consulting </h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <a href="#about" class="btn-get-started scrollto">Read More</a>
+          </div>
+        </div>
+
+      </div>
+
+      <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
+      </a>
+
+      <a class="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
+        <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
+      </a>
+
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= Featured Services Section ======= -->
+    <section id="featured-services" class="featured-services">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row">
+          <div class ="col-md-3">
+                  <div class="text-center"><i class="fas fa-calendar-alt text-center icon"></i></div>
+                  <p class="title text-center mt-3">Total Number of Years</p>
+                  <p class="description text-center">16 +</p>
+          </div>
+          <div class ="col-md-3">
+                  <div class="text-center"><i class="fas fa-people-arrows icon"></i></div>
+                  <p class="title text-center mt-3">Clients</p>
+                  <p class="description text-center">78000</p>
+          </div>
+          <div class ="col-md-3">
+                  <div class="text-center"><i class="fas fa-users icon"></i></div>
+                  <p class="title text-center mt-3">Customers</p>
+                  <p class="description text-center">490</p>
+          </div>
+          <div class ="col-md-3">
+                  <div class="text-center"><i class="fas fa-hourglass-start icon"></i></div>
+                  <p class="title text-center mt-3">Hours of Development</p>
+                  <p class="description text-center">42000</p>
+          </div>
+
+      </div>
+    </section><!-- End Featured Services Section -->
+
+    <!-- ======= Cta Section ======= -->
+    <!-- <section id="cta" class="cta">
+      <div class="container" data-aos="zoom-in">
+
+        <div class="text-center">
+          <h3>In an emergency? Need help now?</h3>
+          <p> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <a class="cta-btn scrollto" href="#appointment">Make an Make an Appointment</a>
+        </div>
+
+      </div>
+    </section> -->
+    <!-- End Cta Section -->
+
+    <!-- ======= About Us Section ======= -->
+    <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+        <div class="row">
+          <div class="col-lg-6" data-aos="fade-right">
+            <h2>Why choose Us?</h2>
+            <p class="choose">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+               Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. </p>
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 content" data-aos="fade-left">
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/2Gv6CMvI-Lw?si=T9HEdxTunBfOk1Sm" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Us Section -->
+
+    <!-- ======= Service Section ======= -->
+    <section id="service" class="service">
+      <div class="container" data-aos="fade-up">
+        <div class="row">
+          <h3>Services We Offer
+          </div>
+
+        <div class="row no-gutters">
+          <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch service-bg">
+            <div class="count-box">
+              <p class="text-center"><strong>ERP Solution</strong></p>
+              </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch service-bg">
+            <div class="count-box">
+            <p class="text-center"><strong>Web Applications</strong></p>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch service-bg">
+            <div class="count-box">
+            <p class="text-center"><strong>E-Commerce</strong></p>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch service-bg">
+            <div class="count-box">
+            <p class="text-center"><strong>Odoo Customization </strong></p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Counts Section -->
+  
+      <!-- ======= odoo Section ======= -->
+    <section id="odoo" class="odoo">
+      <div class="container" data-aos="fade-up">
+        <div class="row">
+        <div class="flex">
+          <img class="odooimg" src="assets/img/slide/odoo.png">
+            <h3 class="d-inline">Odoo Customizations</h3>
+          </div>
+          </div>
+          <div class="row">
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+              when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+              It has survived not only five centuries, but also the leap into electronic typesetting,
+               remaining essentially unchanged.</p>
+
+          </div>
+
+      </div>
+    </section><!-- End odoo Section -->
+
+    <!-- ======= ERP Section ======= -->
+    <section id="odoo" class="odoo">
+      <div class="container" data-aos="fade-up">
+        <div class="row">
+        <div class="flex">
+          <img class="odooimg" src="assets/img/slide/erp.png">
+            <h3 class="d-inline">ERP Solutions for your Business </h3>
+          </div>
+          </div>
+          <div class="row">
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+              when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+              It has survived not only five centuries, but also the leap into electronic typesetting,
+               remaining essentially unchanged.</p>
+
+          </div>
+
+      </div>
+    </section><!-- End ERP Section -->
+
+       <!-- ======= Web Application Section ======= -->
+       <section id="odoo" class="odoo">
+      <div class="container" data-aos="fade-up">
+        <div class="row">
+        <div class="flex">
+          <img class="odooimg" src="assets/img/slide/mobile.png">
+            <h3 class="d-inline">Mobile Application Development </h3>
+          </div>
+          </div>
+          <div class="row">
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+              when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+              It has survived not only five centuries, but also the leap into electronic typesetting,
+               remaining essentially unchanged.</p>
+
+          </div>
+
+      </div>
+    </section><!-- Web Application Section -->
+         <!-- ======= Super Fast service Section ======= -->
+         <section id="odoo" class="odoo">
+      <div class="container" data-aos="fade-up">
+        <div class="row">
+        <div class="flex">
+          <img class="odooimg" src="assets/img/slide/support.png">
+            <h3 class="d-inline">Super-fast Support Service </h3>
+          </div>
+          </div>
+          <div class="row">
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+              when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+              It has survived not only five centuries, but also the leap into electronic typesetting,
+               remaining essentially unchanged.</p>
+
+          </div>
+
+      </div>
+    </section><!-- super fast service Section -->
+    </section><!-- Web Application Section -->
+         <!-- ======= Industires Section ======= -->
+         <section id="odoo" class="odoo">
+      <div class="container" data-aos="fade-up">
+        <div class="row">
+        <div class="flex">
+          <img class="odooimg" src="assets/img/slide/industry.png">
+            <h3 class="d-inline">Industries we serve  </h3>
+          </div>
+          </div>
+          <div class="row">
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+              when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+              It has survived not only five centuries, but also the leap into electronic typesetting,
+               remaining essentially unchanged.</p>
+              </div>
+            </div>
+    </section><!-- Industries Section -->
+  <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+        <div class="row mt-5">
+          <div class="col-md-6">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3904.2708937688285!2d75.36979497472649!3d11.886215637744886!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba43d3c71ce40bd%3A0x48feff0b494c0b6f!2sCasa%20Marina%20Complex%2C%20Chettipedika%2C%20Kannur%2C%20Kerala%20670002!5e0!3m2!1sen!2sin!4v1693834362072!5m2!1sen!2sin" width="550" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+          <div class="col-md-6">
+             <div class="row">
+                  <div class="col-md-6">
+                  <h4 class="text-center text-white">Feature Products</h4>
+             <ul class="footer-page">
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
+            </ul>
+                   </div>
+               <div class="col-md-6">
+               <h4 class="service-link text-white">Our Services</h4>
+            <ul class="footer-page">
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
+            </ul>
+               </div>
+              </div>
+              <div class="row social">
+              <div class="footer-social mt-4">
+                    <a href="https://www.whatsapp.com/" target="_blank">
+                        <img src="assets/img/social-icons/whatsapp.png" alt="whatsapp"></a>
+                    <a href="https://www.instagram.com/" target="_blank">
+                        <img src="assets/img/social-icons/instagram.png" alt="instagram"></a>
+                    <a href="https://www.facebook.com/" target="_blank">
+                        <img src="assets/img/social-icons/facebook.png" alt="facebook"></a>
+                    <a href="https://twitter.com/" target="_blank">
+                        <img src="assets/img/social-icons/twitter.png" alt="twitter"></a>
+                        <a href="https://www.youtube.com/" target="_blank">
+                        <img src="assets/img/social-icons/youtube.png" alt="youtube"></a>
+                </div>
+             </div>
+            </div>
+           
+          </div>
+        </div>
+        
+
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+           <div class="col-lg-3 col-md-6">
+            <div class="footer-info">
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright text-white">
+        &copy; Copyright <strong><span>Mywork</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits text-white">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/medicio-free-bootstrap-theme/ -->
+        Designed by <a href="#">Balasaravanan</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
